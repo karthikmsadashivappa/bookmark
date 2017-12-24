b5c1ed48bf36a2efa32c401e62b80866263f99d6
Karthik M S <ms18karthik@gmail.com>
2017-12-24 17:33:45 +0530

    Added visit and delete functionality

--- js/main.js
+++ js/main.js
@@ -42,11 +42,43 @@ if(localStorage.getItem('bookmarks') === null){
 }
 
 
+function deleteBookmark(url){
+
+	var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+
+	for(var i=0;i < bookmarks.length; i++){
+		if(bookmarks[i].url == url){
+			bookmarks.splice(i,1);
+		}
+	}
+
+	localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+
+	fetchBookmarks();
+}
+
 function fetchBookmarks(){
 
 	var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
 
-	console.log(bookmarks);
+	var bookmarksResults = document.querySelector('#bookmarksResults');
+
+	bookmarksResults.innerHTML = '';
+
+	for(var i=0; i<bookmarks.length;i++){
+			var name = bookmarks[i].name;
+			var url = bookmarks[i].url;
+
+	bookmarksResults.innerHTML += '<div class="well">'+
+								   '<h3>'+name+
+								   ' <a class="btn btn-default" target="_blank" href="'+url+'">Visit</a>'+
+								   ' <a onclick="deleteBookmark(\''+url+'\')" class="btn btn-danger" href="#">Delete</a>'+
+
+								   '</h3>'+
+								   '</div>';
+
+	}
+
 }
 
 
